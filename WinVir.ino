d7fb922a82f4b808e18e46c93e190c5402f0ac68
clippy192 <clippy@clippyco.com>
2022-12-20 18:36:39 -0600

    Update WinVir.ino
    actually removing mouse method

--- WinVir.ino
+++ WinVir.ino
@@ -1,16 +1,10 @@
-#include <Mouse.h>
 #include <Keyboard.h>
 
 long randomdelay;
-//long mousedelay;
-//int mousemove;
 int picker;
 float ms;
 float mins;
 long secs;
-int mousex;
-int mousey;
-int mousect;
 int tabret;
 
 String stringOne = "m: Next boi is in ";
@@ -19,11 +13,10 @@ String stringThree = "/60, so it'll be a ";
 String stringFour = "Annoyance is at ";
 
 // how annoying you want this script to be, lower is more annoying. recommended between 1 and 5
-int annoyance = 1                   ;
+int annoyance = 1;
 
 void setup() {
-  Keyboard.begin(); //start emulating mouse
-  Mouse.begin(); //start emulating keyboard
+  Keyboard.begin(); //start emulating keyboard
   delay(10000);
   Serial.begin(9600);
   Serial.print(stringFour);
@@ -35,18 +28,12 @@ void loop() {
   //initial seed and starting values for shit modified by annoyance factor
   randomSeed(analogRead(3));
   randomdelay = random(5000, 30000); //delay between each annoyance method
-  //mousedelay = random(100, 2000); //delay between each individual mouse movement in method 6
-  //mousemove = random(10, 100); //how many times to actually move the mouse
 
   ms = millis(); //keeps track of how long the script has been running
   mins = ms / 60000;
   picker = random(1, 50); //used to choose which method to annoy the user with.
   tabret = random(1, 16); //used to press tab a certain amount of times in method 5
-
   randomdelay = annoyance * randomdelay;
-  //mousedelay = annoyance * mousedelay;
-  //mousemove = mousemove / annoyance;
-
   secs = randomdelay / 1000;
 
   //these next few lines are used for debugging.
@@ -56,11 +43,10 @@ void loop() {
   else if (picker < 20) Serial.println("backboi.");
   else if (picker < 30) Serial.println("altboi.");
   else if (picker < 40) Serial.println("winboi.");
-  else if (picker < 50) {
+  else {
     Serial.print("tab-boi. should press tab this many times: ");
     Serial.println(tabret);
   }
-  else Serial.println("mouseboi.");
 
   delay(randomdelay); //delays the time set above before pressing a key
 
@@ -86,25 +72,4 @@ void loop() {
     }
     Keyboard.write(KEY_RETURN); //loop until "i" less than tabret, then press enter.
   }
-  
-// may remove this, its never really worked right
-//  else { //any other number, move mouse in a random direction.
-//    Serial.println("Just fuck my shit up fam.");
-//    mousect = 0;
-//    do {
-//      randomSeed(analogRead(3)); //rerandomize seed every loop. this doesn't really work. it's too predictable.
-//      mousex = random(-200, 200); //mousex and mousey are used to decide how far to move the mouse horizontally and vertically.
-//      mousey = random(-200, 200);
-//
-//      delay(mousedelay * annoyance);
-//
-//      //used for debugging
-//      Serial.print(mousex);
-//      Serial.print(", ");
-//      Serial.println(mousey);
-//
-//      Mouse.move(mousex, mousey); //actually move the mouse
-//      mousect = mousect + 1; //increment mousect by 1 so the script knows when to stop moving the mouse.
-//    } while ( mousect < (mousemove)); //when mousect equals mousemove, stop looping and restart the script.
-//  }
 }
